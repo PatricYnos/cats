37b1aafbd3af4125737df0d413100b57c4023229
Alexey Permyakov <patric.ynos@gmail.com>
2024-08-28 18:14:00 +0300

    start project

---
+++ src/pages/main/main.jsx
@@ -0,0 +1,16 @@
+export default function Main() {
+  return (
+    <>
+      <div className="header">
+        <div className="logo"></div>
+        <div className="burger"></div>
+      </div>
+      <div className="hero"></div>
+      <div className="advantages"></div>
+      <div className="rooms"></div>
+      <div className="review"></div>
+      <div className="place"></div>
+      <div className="footer"></div>
+    </>
+  )
+}
